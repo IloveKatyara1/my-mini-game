18cc6f25cc7a142f8cda6068caad17115f8c770b
Nazar Tsventarnyi <110458518+IloveKatyara1@users.noreply.github.com>
2023-07-23 00:53:07 +0300

    added Player

---
+++ MiniGame/MiniGame/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniGame
+{
+    internal class Player
+    {
+        private int Health = 100;
+        private int Armor = 0;
+        private int Damage = 10;
+
+        public void TakeDamage(int damage)
+        {
+            damage -= Armor;
+
+            if (damage <= 0)
+               damage = 0;
+            else if (damage > Health)
+               damage = Health;
+
+            Health -= damage;
+        }
+
+        public void HealHealth(int heal)
+        {
+            int Different = 100 - Health;
+
+            if (Different < heal)
+               heal = Different;
+
+            Health += heal;
+        }
+
+        public string Statistic()
+        {
+            return $"Health: {Health} / 100 \nArmor: {Armor} \nDamage: {Damage}";
+        }
+
+        public int ShowDamage()
+        {
+            return Damage;
+        }
+    }
+}
---
+++ MiniGame/MiniGame/Program.cs
@@ -0,0 +1,8 @@
+﻿using MiniGame;
+
+MainLogic mainLogic = new ();
+
+Console.WriteLine("Chapter 1");
+Console.WriteLine("when you got up, you saw that you are in dungeon, and you must go out from here!");
+
+mainLogic.ChooseAction();
